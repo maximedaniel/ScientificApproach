05238ff72a070cfc36c6fc3e46e22f3d412b2e4b
maximedaniel <maxaxeldaniel@gmail.com>
2020-01-27 17:31:50 +0100

    Adding content

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ import ScammperrDonutChart from './components/ScammperrDonutChart'
 import BrainWritingGraph from './components/BrainWritingGraph'
 import SwotAnalysisMatrix from './components/SwotAnalysisMatrix'
 import ModelCanvasTreeMap from './components/ModelCanvasTreeMap'
+import EisenhowerDecisionMatrix from './components/EisenhowerDecisionMatrix'
+import TrizWindowsMatrix from './components/TrizWindowsMatrix'
+import SixThinkingHatsGraph from './components/SixThinkingHatsGraph'
 import WhysGraph from './components/WhysGraph'
+import BioStatGvMatrix from './components/BioStatGvMatrix'
 import {ParentSize} from '@vx/responsive'
 
 interface Props{
@@ -38,68 +42,31 @@ class App extends  Component<Props, State> {
                   </Header.Subheader>
                 </Header>
           </Grid.Column>
-          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
-                <Header as="h2" textAlign='left' >1. Vision</Header>
-                <Form>
-                  <TextArea placeholder='I pursued the vision of Tangible User Interfaces.' />
-                </Form>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    1. Vision
+                  </Header.Content>
+                  <Header.Subheader>
+                    How do you envision HCI in the future?
+                  </Header.Subheader>
+                </Header>
+                {/*<p><b>Example:</b> I envision Programmable Matter.
+                Programmable Matter is defined as a cyber-physical conjugation. This technology would enable a synthetic reality; an environment in which all the objects in a users environment (including the ones inserted by the computer) are physically realized.
+                Programmable Matter promises to bridge the gap between the physicality of Tangible User Interfaces (TUIs) and the flexibility of Graphical User Interfaces (GUIs).
+                </p>*/}
           </Grid.Column>
-          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
-                <Header as="h2" textAlign='left'>2. Motivation</Header>
-                <Form>
-                  <TextArea placeholder='I focused on societal challenges regarding Energy Demand-Side Managment (e.g., energy demand saving, energy demand shifting).' />
-                </Form>
-          </Grid.Column>
-          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
-                <Header as="h2" textAlign='left'>3. Question</Header>
-                <Form>
-                  <TextArea placeholder='I asked, how can Tangible User Interfaces help in Energy Demand-Side Managment?' />
-                </Form> 
-          </Grid.Column>
-          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
-                <Header as="h2" textAlign='left'>4. State of the Art</Header>
-                <Card>
-                  <Card.Content>
-                    <Card.Header>Systematic Litterature Review
-                    </Card.Header>
-                    <Card.Meta>Moher et al. (2009)</Card.Meta>
-                    <Card.Description>
-                    <Form>
-                    <Label as='a'  color='green' content='Reproductive' icon='plus' horizontal/>
-                    <Label as='a' color='red' content='Subjective' icon='minus' horizontal/>
-                    </Form>
-                    <br/>
-                    <Form>
-                        <Label attached='top left'>1. Identification</Label>
-                        <TextArea placeholder='I gathered the 1000th articles from 2005 to 2015 using keywords (HCI, Energy Demand-Side Managment, and Energy Saving) in the databases (Google Scholar and ACM).' />
-                    </Form>
-                    <br/>
-                    <Form>
-                        <Label attached='top left'>2. Exclusion</Label>
-                        <TextArea placeholder='I excluded articles that are duplicated, are not about the subject in the title and the abstract, lack description and/or illustration of the prototype.' />
-                    </Form>
-                    <br/>
-                    <Form>
-                        <Label attached='top left'>3. Evaluation</Label>
-                        <TextArea placeholder='I assessed articles using 15 criteria (targeted social space, temporal scale of data, persuasion functions, type of interface, etc.) organized in four classes (Context, User, Interface, System)' />
-                    </Form>
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-          </Grid.Column>
-          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
-               <Header as="h2" textAlign='left'>5. Hypothesis</Header>
-          </Grid.Column>
+          
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
                     <Card.Content>
-                      <Card.Header>Discovery Matrix
+                      <Card.Header>TRIZ 9-windows
                       </Card.Header>
-                      <Card.Meta>Moher (1954)</Card.Meta>
+                      <Card.Meta>Genrich Altshuller (1984)</Card.Meta>
                       <Card.Description>
                       <ParentSize>
                           {parent => (
-                              <DiscoveryMatrixHeatMap 
+                              <TrizWindowsMatrix
                                   width={parent.width}
                                   height={300}
                                   parentRef={parent.ref}
@@ -110,25 +77,27 @@ class App extends  Component<Props, State> {
                     </Card.Content>
                   </Card>      
           </Grid.Column>
-          <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
-                <Card fluid>
-                    <Card.Content>
-                      <Card.Header>MindMap
-                      </Card.Header>
-                      <Card.Meta>Tony Buzan (1974)</Card.Meta>
-                      <Card.Description>
-                      <ParentSize>
-                          {parent => (
-                              <MindMapGraph 
-                                  width={parent.width}
-                                  height={300}
-                                  parentRef={parent.ref}
-                              />
-                          )}
-                      </ParentSize>
-                      </Card.Description>
-                    </Card.Content>
-                  </Card>      
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    2. Challenge
+                  </Header.Content>
+                  <Header.Subheader>
+                    What does challenge the advent of this vision?
+                  </Header.Subheader>
+                </Header>
+                {/*<p><b>Example:</b> Need to demonstrate the feasability, utility and usability <i>in situ</i> of new form factors for Shape-Changing Interfaces.
+                </p>*/}
+          </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    3. Question
+                  </Header.Content>
+                  <Header.Subheader>
+                    Could you give answers to a challenge e.g. in regards to a societal issue?
+                  </Header.Subheader>
+                </Header>
           </Grid.Column>
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
@@ -150,6 +119,36 @@ class App extends  Component<Props, State> {
                     </Card.Content>
                   </Card>      
           </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    7. Hypothesis & Prediction
+                  </Header.Content>
+                  <Header.Subheader>
+                  </Header.Subheader>
+                </Header>
+          </Grid.Column>
+          <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
+                <Card fluid>
+                    <Card.Content>
+                      <Card.Header>Discovery Matrix
+                      </Card.Header>
+                      <Card.Meta>Moher (1954)</Card.Meta>
+                      <Card.Description>
+                      <ParentSize>
+                          {parent => (
+                              <DiscoveryMatrixHeatMap 
+                                  width={parent.width}
+                                  height={300}
+                                  parentRef={parent.ref}
+                              />
+                          )}
+                      </ParentSize>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>      
+          </Grid.Column>
+          
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
                     <Card.Content>
@@ -190,6 +189,27 @@ class App extends  Component<Props, State> {
                     </Card.Content>
                   </Card>      
           </Grid.Column>
+          
+          <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
+                <Card fluid>
+                    <Card.Content>
+                      <Card.Header>MindMap
+                      </Card.Header>
+                      <Card.Meta>Tony Buzan (1974)</Card.Meta>
+                      <Card.Description>
+                      <ParentSize>
+                          {parent => (
+                              <MindMapGraph 
+                                  width={parent.width}
+                                  height={300}
+                                  parentRef={parent.ref}
+                              />
+                          )}
+                      </ParentSize>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>      
+          </Grid.Column>
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
                     <Card.Content>
@@ -213,6 +233,65 @@ class App extends  Component<Props, State> {
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
                     <Card.Content>
+                      <Card.Header>Six Thinking Hats
+                      </Card.Header>
+                      <Card.Meta>Edward de Bono (1985)</Card.Meta>
+                      <Card.Description>
+                      <ParentSize>
+                          {parent => (
+                              <SixThinkingHatsGraph
+                                  width={parent.width}
+                                  height={300}
+                                  parentRef={parent.ref}
+                              />
+                          )}
+                      </ParentSize>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>      
+          </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    8. Experimentation
+                  </Header.Content>
+                  <Header.Subheader>
+                  </Header.Subheader>
+                </Header>
+          </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    8. Analysis
+                  </Header.Content>
+                  <Header.Subheader>
+                  </Header.Subheader>
+                </Header>
+          </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='center'>
+                <Card fluid>
+                    <Card.Content>
+                      <Card.Header>Inferential Statistic Matrix
+                      </Card.Header>
+                      <Card.Meta>BiostaTGV (2020)</Card.Meta>
+                      <Card.Description>
+                      <ParentSize>
+                          {parent => (
+                              <BioStatGvMatrix 
+                                  width={parent.width}
+                                  height={300}
+                                  parentRef={parent.ref}
+                              />
+                          )}
+                      </ParentSize>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>      
+          </Grid.Column>
+
+          <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
+                <Card fluid>
+                    <Card.Content>
                       <Card.Header>SWOT Analysis
                       </Card.Header>
                       <Card.Meta>George Albert Smith and C Roland Christiensen (1960)</Card.Meta>
@@ -230,12 +309,50 @@ class App extends  Component<Props, State> {
                     </Card.Content>
                   </Card>      
           </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    9. Dissemination
+                  </Header.Content>
+                  <Header.Subheader>
+                  </Header.Subheader>
+                </Header>
+          </Grid.Column>
+          <Grid.Column width={16} style={{paddingTop:'1rem'}} textAlign='left'>
+                <Header as="h2">
+                  <Header.Content>
+                    Others tools
+                  </Header.Content>
+                  <Header.Subheader>
+                  </Header.Subheader>
+                </Header>
+          </Grid.Column>
+          <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='left'>
+            <Card fluid>
+                    <Card.Content>
+                      <Card.Header>Eisenhower Decision Matrix
+                      </Card.Header>
+                      <Card.Meta>Dwight D. Eisenhower (1960)</Card.Meta>
+                      <Card.Description>
+                      <ParentSize>
+                          {parent => (
+                              <EisenhowerDecisionMatrix
+                                  width={parent.width}
+                                  height={300}
+                                  parentRef={parent.ref}
+                              />
+                          )}
+                      </ParentSize>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card> 
+          </Grid.Column>
           <Grid.Column width={8} style={{paddingTop:'1rem'}} textAlign='center'>
                 <Card fluid>
                     <Card.Content>
-                      <Card.Header>SWOT Analysis
+                      <Card.Header>Business Model Canvas
                       </Card.Header>
-                      <Card.Meta>George Albert Smith and C Roland Christiensen (1960)</Card.Meta>
+                      <Card.Meta>Alexander Osterwalder (2008)</Card.Meta>
                       <Card.Description>
                       <ParentSize>
                           {parent => (
@@ -250,10 +367,6 @@ class App extends  Component<Props, State> {
                     </Card.Content>
                   </Card>      
           </Grid.Column>
-            <Header as="h4" textAlign='left'>6. Prediction</Header>
-            <Header as="h4" textAlign='left'>7. Experimentation</Header>
-            <Header as="h4" textAlign='left'>8. Analysis</Header>
-            <Header as="h4" textAlign='left'>9. Dissemination</Header>
         </Grid.Row>
       </Grid>
     );
--- src/components/ModelCanvasTreeMap.tsx
+++ src/components/ModelCanvasTreeMap.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import * as d3 from 'd3'
+import { defaultCipherList } from 'constants'
 
 interface Props{width:any,height:any,parentRef: HTMLDivElement | null}
 interface State{height:number,width:number}
@@ -25,48 +26,88 @@ class ModelCanvasTreeMap extends  Component<Props, State> {
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-    var format = d3.format(",d");
-    var treemap = data => d3.treemap()
-    .tile(tile)
-    size([width, height])
-    .padding(1)
-    .round(true)(d3.hierarchy(data).sum(d => d.value).sort((a, b) => b.value - a.value))
-    const root = treemap(data);
+    var nodes = [
+      {id:0, x:width/5, y:0},
+      {id:1, x:width/5 * 2, y:0},
+      {id:2, x:width/5 * 3, y:0},
+      {id:3, x:width/5 * 4, y:0},
+      {id:4, x:width/5, y:height/4*3},
+      {id:5, x:width/5 * 2, y:height/4*3},
+      {id:6, x:width/5 * 3, y:height/4*3},
+      {id:7, x:width/5 * 4, y:height/4*3},
+      {id:8, x:width/5, y:height/4*1.5},
+      {id:9, x:width/5 * 2, y:height/4*1.5},
+      {id:10, x:width/5 * 3, y:height/4*1.5},
+      {id:11, x:width/5 * 4, y:height/4*1.5},
+      {id:12, x:0, y:height/4*3},
+      {id:13, x:width, y:height/4*3},
+      {id:14, x:width/2, y:height/4*3},
+      {id:15, x:width/2, y:height},
+    ]
+    var links = [
+      {from:0, to:4},
+      {from:1, to:5},
+      {from:2, to:6},
+      {from:3, to:7},
+      {from:8, to:9},
+      {from:10, to:11},
+      {from:12, to:13},
+      {from:14, to:15}
+    ]
 
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height])
-        .style("font", "10px sans-serif");
-  
-    const leaf = svg.selectAll("g")
-      .data(root.leaves())
-      .join("g")
-        .attr("transform", d => `translate(${d.x0},${d.y0})`);
-  
-    leaf.append("title")
-        .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${format(d.value)}`);
-  
-    leaf.append("rect")
-        .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
-        .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
-        .attr("fill-opacity", 0.6)
-        .attr("width", d => d.x1 - d.x0)
-        .attr("height", d => d.y1 - d.y0);
-  
-    leaf.append("clipPath")
-        .attr("id", d => (d.clipUid = DOM.uid("clip")).id)
-      .append("use")
-        .attr("xlink:href", d => d.leafUid.href);
-  
-    leaf.append("text")
-        .attr("clip-path", d => d.clipUid)
-      .selectAll("tspan")
-      .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(format(d.value)))
-      .join("tspan")
-        .attr("x", 3)
-        .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
-        .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
-        .text(d => d);
+    var texts = [
+      {text:'KEY', x:width/5 - width/10, y:0},
+      {text:'PARTNERS', x:width/5 - width/10, y:fontSize},
+      {text:'KEY', x:(width/5 * 2) - width/10, y:0},
+      {text:'ACTIVITIES', x:(width/5 * 2) - width/10, y:fontSize},
+      {text:'VALUE', x:(width/5 * 3) - width/10, y:0},
+      {text:'PROPOSITIONS', x:(width/5 * 3) - width/10, y:fontSize},
+      {text:'CUSTOMER', x:(width/5 * 4) - width/10, y:0},
+      {text:'RELATIONSHIPS', x:(width/5 * 4) - width/10, y:fontSize},
+      {text:'CUSTOMER', x:width - width/10, y:0},
+      {text:'SEGMENTS', x:width - width/10, y:fontSize},
+      {text:'KEY', x:(width/5 * 2) - width/10, y:height/4*1.5},
+      {text:'RESSOURCES', x:(width/5 * 2) - width/10, y: height/4*1.5 + fontSize},
+      {text:'CHANNELS', x:(width/5 * 4) - width/10, y:height/4*1.5},
+      {text:'COST STRUCTURES', x:0 + width/4, y:height/4*3},
+      {text:'REVENUE STREAMS', x:width - width/4, y:height/4*3},
+    ]
+    
+    var getNodeFromId = (nodeId) => nodes.filter((node) => node.id === nodeId)[0];
+
+    svg.selectAll()
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("x1", (link) => getNodeFromId(link.from).x)
+    .attr("y1", (link) => getNodeFromId(link.from).y)
+    .attr("x2", (link) => getNodeFromId(link.to).x)
+    .attr("y2", (link) => getNodeFromId(link.to).y)
+    .attr("stroke","#f7931e")
+    .attr("stroke-width",lineStrokeWidth);
+
+    svg.selectAll()
+    .data(texts)
+    .enter()
+    .append("text")
+    //@ts-ignore
+    .attr("x", d => d.x)
+    //@ts-ignore
+    .attr("y", d => d.y  + textStrokeWidth)
+    //@ts-ignore
+    .text( d => d.text)
+    .attr("fill", "white")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "text-before-edge")
+    .attr("font-size",  fontSize)
+    .attr("paint-order", "stroke")
+    .attr("stroke","#f7931e")
+    .attr("stroke-width", lineStrokeWidth)
+    .attr("stroke-linecap", "butt")
+    .attr("stroke-linejoin", "miter")
+    .attr("font-weight", "800");
+
+
   }
 
   componentDidMount(){
